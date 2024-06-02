7722a75a9dfb92457c1dba2978033c6eb19f22ac
Aamir Azad <aamirmazad@gmail.com>
2024-06-02 17:16:26 -0400

    fix: immich preloaded but not used

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,45 +1,39 @@
 "use client";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
-import { buttonVariants } from "~/components/ui/button";
+import { Button, buttonVariants } from "~/components/ui/button";
 import Tooltip from "./tooltip";
 import { ModeToggle } from "./mode-toggle";
 
 export function TopNav() {
   return (
-    <nav className="m-4 flex h-10 flex-wrap items-center justify-between rounded-bl-md rounded-tl-md dark:bg-slate-900 bg-slate-200 text-xl">
+    <nav className="m-4 flex h-10 flex-wrap items-center justify-between rounded-bl-md rounded-tl-md bg-slate-200 text-xl dark:bg-slate-900">
       <div className="flex w-64">
         <Link
           href="/"
-          className={`${buttonVariants({ variant: "link" })} text-xl font-bold `}
+          className={`${buttonVariants({ variant: "link" })} text-xl font-bold`}
         >
           Homelab Connector
         </Link>
       </div>
-      <div className="flex items-center divide-x divide-dotted rounded-br-md rounded-tr-md dark:bg-slate-700 bg-slate-300">
-        <Link
-          href="paperless"
-          className={buttonVariants({ variant: "link" })}
-        >
+      <div className="flex items-center divide-x divide-dotted rounded-br-md rounded-tr-md bg-slate-300 dark:bg-slate-700">
+        <Link href="paperless" className={buttonVariants({ variant: "link" })}>
           Paperless-ngx
         </Link>
         <div>
           <Tooltip text="Comming soon!">
-            <Link
-              href="/immich"
-              className={`${buttonVariants({ variant: "link" })} pointer-events-none`}
-              style={{ opacity: 0.5 }}
+            <Button
+              variant="link"
+              className="pointer-events-none opacity-50"
               aria-disabled="true"
               tabIndex={-1}
             >
               Immich
-            </Link>
+            </Button>
           </Tooltip>
         </div>
         <div>
-          <div
-            className={buttonVariants({ variant: "link" })}
-          >
+          <div className={buttonVariants({ variant: "link" })}>
             <SignedOut>
               <SignInButton />
             </SignedOut>
