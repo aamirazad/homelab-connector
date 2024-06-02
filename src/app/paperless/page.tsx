fa30b139f384ec7c340b2523a622d79b1ba88643
Aamir Azad <aamirmazad@gmail.com>
2024-06-02 14:47:14 -0400

    feat: skeleton paperless page

---
+++ src/app/paperless/page.tsx
@@ -0,0 +1,10 @@
+
+export default function PaperlessPage() {
+    return (
+        <main className="p-4 px-6">
+            <div className="flex flex-col items-center justify-center">
+                <p>Welcome to Paperless-ngx</p>  
+            </div>
+        </main>
+    );
+}
