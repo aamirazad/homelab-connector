88eab1a53c3ccab35b6ef73d86eab5bb1a3942a4
Aamir Azad <aamirmazad@gmail.com>
2024-06-02 14:46:37 -0400

    feat: add to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import { SignInButton } from "@clerk/nextjs";
 import Link from "next/link";
+import { ExternalLink } from 'lucide-react';
+
 
 export default function HomePage() {
   return (
     <main className="p-4 px-6">
       <div className="flex flex-col items-center justify-center">
-        <div>Welcome to Homelab Connector</div>
+        <p>Welcome to Homelab Connector</p>
+        <p>Check out the <a rel="noopener noreferrer" target="_blank" className="underline text-blue-600 hover:text-blue-800" href="https://github.com/aamirazad/homelab-connector/blob/main/README.md">README<ExternalLink size={16} className="ml-1 inline-block" /></a>  to get started.</p>
+        <p>Or sign in to access the dashboard.</p>
       </div>
     </main>
   );
