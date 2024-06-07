d8c5f4e3c1d20ad8263714d52f4aef9b9071a75a
Aamir Azad <aamirmazad@gmail.com>
2024-06-07 12:11:25 -0400

    Style changes

--- src/app/setup/page.tsx
+++ src/app/setup/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { db } from "~/server/db";
-import { Button } from "~/components/ui/button"
+import { Button } from "~/components/ui/button";
 import {
   Form,
   FormControl,
@@ -11,15 +11,15 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "~/components/ui/form"
-import { Input } from "~/components/ui/input"
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
 
 function FullName() {
-  return <div>FullName</div>;
+  return <div>Hello Dathi</div>;
 }
 
 function PaperlessURL() {
-  return <div>PaperlessURL</div>;
+  return <div>It's magic</div>;
 }
 
 function PaperlessAPI() {
@@ -38,26 +38,26 @@ export default function userSetup() {
   const formElements = [
     <FullName />,
     <PaperlessURL />,
-    <PaperlessAPI />
+    <PaperlessAPI />,
     // <PaperlessURL data={data} handleChange={handleChange} />,
     // <PaperlesssKey data={data} setData={setData} />,
   ];
 
   return (
-    <div>
+    <div className="flex flex-col gap-6">
       <div>{formElements[activeTab]}</div>
       <div className="mx-auto flex flex-wrap gap-x-6">
         <button
           disabled={activeTab === 0 ? true : false}
           onClick={() => setActiveTab((prev) => prev - 1)}
-          className={`rounded-xl bg-blue-600 px-4 py-2 text-white ${activeTab === 0 ? "bg-slate-600 opacity-50" : "opacity-100"}`}
+          className={`rounded-xl bg-green-600 px-4 py-2 text-white ${activeTab === 0 ? "bg-slate-600 opacity-50" : "opacity-100"}`}
         >
           Back
         </button>
         <button
           disabled={activeTab === formElements.length - 1 ? true : false}
           onClick={() => setActiveTab((prev) => prev + 1)}
-          className={`rounded-xl bg-blue-600 px-4 py-2 text-white ${activeTab === formElements.length - 1 ? "bg-slate-600 opacity-50" : "opacity-100"}`}
+          className={`rounded-xl bg-blue-800 px-4 py-2 text-white ${activeTab === formElements.length - 1 ? "bg-slate-600 opacity-50" : "opacity-100"}`}
         >
           Next
         </button>
