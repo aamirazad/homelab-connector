ca8a158dfc0e45832e89e1bdf2acc30580321bf2
Aamir Azad <aamirmazad@gmail.com>
2024-06-25 16:04:07 +0530

    remove unused dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,6 @@
  * for Docker builds.
  */
 await import("./src/env.js");
-import path from "path";
-import CopyPlugin from "copy-webpack-plugin";
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -20,25 +18,6 @@ const config = {
         hostname: "img.clerk.com",
       },
     ],
-  },
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        canvas: "./empty-module.ts",
-      },
-    },
-  },
-  webpack: (config) => {
-    config.plugins.push(
-      new CopyPlugin({
-        patterns: [
-          {
-            from: require.resolve("pdfjs-dist/build/pdf.worker.min.js"),
-            to: path.join(__dirname, "public/static/js"),
-          },
-        ],
-      }),
-    );
   },
 };
 
