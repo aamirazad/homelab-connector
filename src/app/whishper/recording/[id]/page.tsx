0fea72541e132e2bbf749a6233f89e052b0bde4d
Aamir Azad <aamirmazad@gmail.com>
2024-10-26 10:16:33 -0400

    chore: update to nextjs v15 (major)

--- src/app/whishper/recording/[id]/page.tsx
+++ src/app/whishper/recording/[id]/page.tsx
@@ -1,10 +1,11 @@
 import AudioPreview from "@/components/audio-preview";
 
-export default function FullAudioPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default async function FullAudioPage(
+  props: {
+    params: Promise<{ id: string }>;
+  }
+) {
+  const params = await props.params;
   return (
     <main className="h-full w-full">
       <AudioPreview id={params.id} />
