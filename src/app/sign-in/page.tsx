b57a036a005920d313c4b685ca3e7cdae6f01f96
Aamir Azad <aamirmazad@gmail.com>
2024-10-26 10:38:00 -0400

    Move to it's own component

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -6,27 +6,33 @@ import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
-export default function SignIn() {
+function SignInContent() {
   const searchParams = useSearchParams();
   const redirect = searchParams.get("redirect");
 
   return (
-    <Suspense>
-      <main className="text-xl">
-        <SignedOut>
-          <LoadingSpinner>Signing you in...</LoadingSpinner>
-          <RedirectToSignIn signInForceRedirectUrl={redirect} />
-        </SignedOut>
-        <SignedIn>
-          You are already signed in
-          <div className="mt-8">
-            Go{" "}
-            <OpenInternalLink href="/" className="text-primary">
-              Home
-            </OpenInternalLink>
-          </div>
-        </SignedIn>
-      </main>
+    <main className="text-xl">
+      <SignedOut>
+        <LoadingSpinner>Signing you in...</LoadingSpinner>
+        <RedirectToSignIn signInForceRedirectUrl={redirect} />
+      </SignedOut>
+      <SignedIn>
+        You are already signed in
+        <div className="mt-8">
+          Go{" "}
+          <OpenInternalLink href="/" className="text-primary">
+            Home
+          </OpenInternalLink>
+        </div>
+      </SignedIn>
+    </main>
+  );
+}
+
+export default function SignIn() {
+  return (
+    <Suspense fallback={<LoadingSpinner>Loading...</LoadingSpinner>}>
+      <SignInContent />
     </Suspense>
   );
 }
