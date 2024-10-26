62c68d01d7fba7f415efdbe77436af94cd595462
Aamir Azad <aamirmazad@gmail.com>
2024-10-26 10:34:42 -0400

    Try adding suspense and fixing type issues

--- src/app/api/getUserData/route.ts
+++ src/app/api/getUserData/route.ts
@@ -2,7 +2,7 @@ import { db } from "@/server/db";
 import { auth } from "@clerk/nextjs/server";
 
 export async function GET() {
-  const { userId } = auth();
+  const { userId } = await auth();
 
   if (!userId) return new Response(`Unauthorized`, { status: 401 });
 
--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -4,26 +4,29 @@ import OpenInternalLink from "@/components/internal-link";
 import LoadingSpinner from "@/components/loading-spinner";
 import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function SignIn() {
   const searchParams = useSearchParams();
   const redirect = searchParams.get("redirect");
 
   return (
-    <main className="text-xl">
-      <SignedOut>
-        <LoadingSpinner>Signing you in...</LoadingSpinner>
-        <RedirectToSignIn signInForceRedirectUrl={redirect} />
-      </SignedOut>
-      <SignedIn>
-        You are already signed in
-        <div className="mt-8">
-          Go{" "}
-          <OpenInternalLink href="/" className="text-primary">
-            Home
-          </OpenInternalLink>
-        </div>
-      </SignedIn>
-    </main>
+    <Suspense>
+      <main className="text-xl">
+        <SignedOut>
+          <LoadingSpinner>Signing you in...</LoadingSpinner>
+          <RedirectToSignIn signInForceRedirectUrl={redirect} />
+        </SignedOut>
+        <SignedIn>
+          You are already signed in
+          <div className="mt-8">
+            Go{" "}
+            <OpenInternalLink href="/" className="text-primary">
+              Home
+            </OpenInternalLink>
+          </div>
+        </SignedIn>
+      </main>
+    </Suspense>
   );
 }
